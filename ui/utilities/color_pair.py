8fd5c7586821213fb526b022f90b436e15131a84
Dmitry <dimaruzaev2@gmai.com>
2023-12-26 20:49:48 +0300

    refactoring color_pair

--- ui/utilities/color_pair.py
+++ ui/utilities/color_pair.py
@@ -1,28 +1,29 @@
 import curses
 
 def setColorPair(color_name):
-    if color_name == 'WHITE':
-        pair_num = 1
-        curses.init_pair(pair_num, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    elif color_name == 'MAGENTA':
-        pair_num = 2
-        curses.init_pair(pair_num, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-    elif color_name == 'YELLOW':
-        pair_num = 3
-        curses.init_pair(pair_num, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    elif color_name == 'RED':
-        pair_num = 4
-        curses.init_pair(pair_num, curses.COLOR_RED, curses.COLOR_BLACK)
-    elif color_name == 'GREEN':
-        pair_num = 5
-        curses.init_pair(pair_num, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    elif color_name == 'BLUE':
-        pair_num = 6
-        curses.init_pair(pair_num, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    elif color_name == 'CYAN':
-        pair_num = 7
-        curses.init_pair(pair_num, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    elif color_name == '*WHITE_ON_RED':
-        pair_num = 100
-        curses.init_pair(pair_num, curses.COLOR_WHITE, curses.COLOR_RED)
+    match color_name:
+        case 'WHITE':
+            pair_num = 1
+            curses.init_pair(pair_num, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        case 'MAGENTA':
+            pair_num = 2
+            curses.init_pair(pair_num, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        case 'YELLOW':
+            pair_num = 3
+            curses.init_pair(pair_num, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        case 'RED':
+            pair_num = 4
+            curses.init_pair(pair_num, curses.COLOR_RED, curses.COLOR_BLACK)
+        case 'GREEN':
+            pair_num = 5
+            curses.init_pair(pair_num, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        case  'BLUE':
+            pair_num = 6
+            curses.init_pair(pair_num, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        case  'CYAN':
+            pair_num = 7
+            curses.init_pair(pair_num, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        case '*WHITE_ON_RED':
+            pair_num = 100
+            curses.init_pair(pair_num, curses.COLOR_WHITE, curses.COLOR_RED)
     return curses.color_pair(pair_num)
